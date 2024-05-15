ccc58d2ce389a10030fa36c63964ec30be3f162b
Chitransh S <chitransh0210@gmail.com>
2024-05-15 06:16:47 +0530

    Debugging code

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(connectionString, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       connectTimeoutMS: 30000, // 30 seconds
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+require('dotenv').config();
 // Connect to MongoDB
 connectDB();
 
